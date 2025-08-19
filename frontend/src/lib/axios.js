3fde9b13767307645589e2fab15daf0fd4ee4a10
DHEERAJ KUMAR <142182380+Dheerajkumar95@users.noreply.github.com>
2025-08-19 12:20:50 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api", 
+  baseURL: "https://newd-7.onrender.com/api", 
   withCredentials: true,
 });
